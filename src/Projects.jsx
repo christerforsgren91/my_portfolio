43856e2305e40bfdc5a4a9f2d34aa1adf071e48b
Christer <christerforsgren91@gmail.com>
2021-04-09 10:55:32 +0200

    added new styling to my projects, a new jsx file ProjectCards and new implementation code, added the projects through a JSON file. Also added new cypress test for that

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+import ProjectCard from './ProjectCard'
+import axios from 'axios'
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id='projects-header'>My Projects</h1>
-      <p>
-        Here you will be able to see my projects.
-      </p>
-    </Container>
-  )
+class Projects extends Component {
+  state = {
+    projects: [],
+    } 
+      componentDidMount() {
+      axios.get('./data/projects.json').then((response) => {
+        this.setState({ projects: response.data})
+      })
+    }
+
+  render() {
+    const { projects } = this.state
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <ProjectCard project={project} />
+        </div>
+      )
+    })
+      
+    return (
+      <Container>
+        <h1 id='projects-header'>My Projects</h1>
+        <Grid>{projectsList}</Grid>
+      </Container>
+    )
+  }
 }
+
+
+  
+
 
 export default Projects
